047376f6444c8728d873efca6e9b54be81c0470f
Kenny Lu <kenny.lu@no.experis.com>
2023-05-08 14:32:15 +0200

    Removed unnecessary file

--- src/main/java/no/experis/backend/repositories/RomanConvertRepository.java
+++
@@ -1,9 +0,0 @@
-package no.experis.backend.repositories;
-import no.experis.backend.models.RomanConvert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RomanConvertRepository extends JpaRepository<RomanConvert,Integer> {
-}
-
--- src/main/java/no/experis/backend/services/romanConvert/RomanConvertService.java
+++
@@ -1,7 +0,0 @@
-package no.experis.backend.services.romanConvert;
-
-import no.experis.backend.models.RomanConvert;
-import no.experis.backend.services.CrudService;
-
-public interface RomanConvertService extends CrudService<RomanConvert,Integer> {
-}
