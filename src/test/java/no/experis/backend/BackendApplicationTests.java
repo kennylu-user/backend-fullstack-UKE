774a51b701837954501ac4abc22f34613594a7f7
Kenny Lu <kenny.lu@no.experis.com>
2023-05-08 13:56:57 +0200

    Made controller tests

--- src/test/java/no/experis/backend/BackendApplicationTests.java
+++ src/test/java/no/experis/backend/BackendApplicationTests.java
@@ -1,13 +1,81 @@
 package no.experis.backend;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.experis.backend.models.ConvertedNr;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BackendApplicationTests {
+	protected MockMvc mvc;
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+			throws JsonParseException, JsonMappingException, IOException {
 
-	@Test
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+	protected void setUp() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@BeforeEach
 	void contextLoads() {
+		setUp();
+	}
+	@Test
+	public void getConvertedNrList() throws Exception {
+		setUp();
+		String uri = "http://localhost:8080/api/v1/convertednr";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	}
+	@Test
+	public void getLogs() throws Exception{
+		String uri = "http://localhost:8080/api/v1/logs";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+
+	}
+	@Test
+	public void getConvertedNrById() throws Exception{
+		String uri = "http://localhost:8080/api/v1/convertednr/X";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+		String str = mvcResult.getResponse().getContentAsString();
+
+		JSONObject jsonObject = new JSONObject(str);
+		String actual = jsonObject.get("id").toString();
+
+
+		assertEquals("X", actual);
+
 	}
 
 }
