451294d3916bf0231e3a854fe195b2ddab72bd51
Kenny Lu <kenny.lu@no.experis.com>
2023-05-12 09:13:55 +0200

    removed unused imports

--- src/test/java/no/experis/backend/BackendApplicationTests.java
+++ src/test/java/no/experis/backend/BackendApplicationTests.java
@@ -1,14 +1,9 @@
 package no.experis.backend;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import no.experis.backend.models.ConvertedNr;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParseException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,23 +12,15 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
-import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BackendApplicationTests {
 	protected MockMvc mvc;
 	@Autowired
 	WebApplicationContext webApplicationContext;
-	protected <T> T mapFromJson(String json, Class<T> clazz)
-			throws JsonParseException, JsonMappingException, IOException {
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		return objectMapper.readValue(json, clazz);
-	}
 	protected void setUp() {
 		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
@@ -44,7 +31,6 @@ class BackendApplicationTests {
 	}
 	@Test
 	public void getConvertedNrList() throws Exception {
-		setUp();
 		String uri = "http://localhost:8080/api/v1/convertednr";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
